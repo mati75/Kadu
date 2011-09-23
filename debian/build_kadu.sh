019b51b6fabf4660747f6ca3ba404941f07cc333
Patryk Cisek <patryk@debian.org>
2011-09-23 13:05:06 +0200

    Tweaked debian/build_kadu.sh to make it fit 0.10

--- debian/build_kadu.sh
+++ debian/build_kadu.sh
@@ -12,15 +12,15 @@ debian_revision=$(echo $version | awk -F "-" '{printf $2}')
 
 git clean -d -f -x
 git reset --hard
-git co 0.9/upstream-kadu
-git br -D 0.9/master || true
-git co -b 0.9/master 0.9/upstream-kadu
-git merge 0.9/upstream-modules 0.9/upstream-themes 0.9/debian
+git co 0.10/upstream-kadu
+git br -D 0.10/master || true
+git co -b 0.10/master 0.10/upstream-kadu
+git merge 0.10/upstream-plugins 0.10/upstream-themes 0.10/debian
 
 if [ "$debian_revision" = 1 ]; then
-    # Generate orig-external{modules,themes}.tar.bz2, but only if it's first
+    # Generate orig-external{plugins,themes}.tar.bz2, but only if it's first
     # Debian revision for particular upstream version
-    tar c externalmodules | bzip2 -9 > ../kadu_$upstream_version.orig-externalmodules.tar.bz2
+    tar c externalplugins | bzip2 -9 > ../kadu_$upstream_version.orig-externalplugins.tar.bz2
     tar c externalthemes | bzip2 -9 > ../kadu_$upstream_version.orig-externalthemes.tar.bz2
 fi
 debuild -us -uc -I.git $@
