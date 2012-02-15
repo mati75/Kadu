da703dc046e17a293f13d50a89dd0317c993fe85
Patryk Cisek <patryk@debian.org>
2012-02-15 18:56:07 +0100

    Updated debian/build_kadu.sh to new branches names

--- debian/build_kadu.sh
+++ debian/build_kadu.sh
@@ -12,10 +12,10 @@ debian_revision=$(echo $version | awk -F "-" '{printf $2}')
 
 git clean -d -f -x
 git reset --hard
-git co 0.11/upstream-kadu
-git br -D 0.11/master || true
-git co -b 0.11/master 0.11/upstream-kadu
-git merge 0.11/upstream-plugins 0.11/upstream-themes 0.11/debian
+git co 0.12/upstream-kadu
+git br -D 0.12/master || true
+git co -b 0.12/master 0.12/upstream-kadu
+git merge 0.12/upstream-plugins 0.12/upstream-themes 0.12/debian
 
 if [ "$debian_revision" = 1 ]; then
     # Generate orig-external{plugins,themes}.tar.bz2, but only if it's first
