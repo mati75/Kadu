9b7f25fbaab19c9bbdf15cb5fe635f6dfcbb0ca9
Patryk Cisek <patryk@debian.org>
2014-09-30 15:58:56 +0200

    Fixed build on kfreebsd-*

--- debian/prepare_install_files.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-if [ "$1" = "linux" ]; then
-    sed -i 's/lib\/kadu/lib\/\*\/kadu/' debian/kadu.install
-elif [ "$1" = "non-linux" ]; then
-    sed -i 's/lib\/\*\/kadu/lib\/kadu/' debian/kadu.install
-fi
-
